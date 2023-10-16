a8a132934e0f9b8352f7e0c01b06ac807241ae0d
curLeeBrace <leeandreimain@gmail.com>
2023-10-16 11:07:13 +0800

    test commit!

--- src/components/Milktea.js
+++ src/components/Milktea.js
@@ -1,7 +1,12 @@
 export const Milktea = () => {
 
     return(
-        <h1>This is Milktea List</h1>
+        <>
+               <h1>This is Milktea List</h1>
+               <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </span>
+        </>
+     
+        
     )
 }
 
